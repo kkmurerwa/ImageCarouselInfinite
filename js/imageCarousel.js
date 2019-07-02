233ef5b184c7c170fb756fd605af9fc9df84b265
xwaxes01 <xwaxes@gmail.com>
2019-07-02 20:45:19 +0300

    Removed all unnecessary code and comments

--- js/imageCarousel.js
+++ js/imageCarousel.js
@@ -1,24 +1,20 @@
-var slideIndex = 0;
+var slideIndex = -1;
 var previousIndex;
 let scroll = document.getElementById("infinitelyLong");
 var slides = document.getElementsByClassName("slideItem");
 var images = document.getElementsByClassName("slideImage");
 
 function plusSlides(n) {
-    // alert("I have been called");
     previousIndex = slideIndex;
     showSlides(slideIndex += n);
-    // alert("previous" + previousIndex + " current" + slideIndex)
 }
 
 function showSlides(n) {
-    // alert(slideIndex);
 
     for (let i = 0; i < slides.length; i++) {
         slides[i].classList.remove("first-div");
         slides[i].classList.remove("second-div");
         slides[i].classList.remove("third-div");
-        // alert(slides[i].classList)
     }
     if (slideIndex > previousIndex) {
         ascending(slides);
@@ -28,19 +24,15 @@ function showSlides(n) {
 }
 
 function ascending(slides) {
-    //Add code here
     if (slideIndex >= 0 && slideIndex <= 3) {
-        // alert(slideIndex);
         slides[slideIndex].classList.add("first-div");
         slides[slideIndex + 1].classList.add("second-div");
         slides[slideIndex + 2].classList.add("third-div");
-    }
-    if (slideIndex == 4) {
+    } else if (slideIndex == 4) {
         slides[4].classList.add("first-div");
         slides[5].classList.add("second-div");
         slides[0].classList.add("third-div");
-    }
-    if (slideIndex == 5) {
+    } else if (slideIndex == 5) {
         slides[5].classList.add("first-div");
         slides[0].classList.add("second-div");
         slides[1].classList.add("third-div");
@@ -53,9 +45,7 @@ function ascending(slides) {
 }
 
 function descending(slides) {
-    // alert(slideIndex);
     if (slideIndex >= 0 && slideIndex <= 3) {
-        // alert(slideIndex);
         slides[slideIndex].classList.add("first-div");
         slides[slideIndex + 1].classList.add("second-div");
         slides[slideIndex + 2].classList.add("third-div");
@@ -67,7 +57,6 @@ function descending(slides) {
         slides[4].classList.add("first-div");
         slides[5].classList.add("second-div");
         slides[0].classList.add("third-div");
-        // slideIndex = 6;
     } else {
         slideIndex = 3
         slides[3].classList.add("first-div");
